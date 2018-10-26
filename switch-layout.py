67016e987248316e7439a5eef766780a97913501
Oleg Yamnikov <bgeyts667@gmail.com>
2018-10-26 14:33:20 +0500

    Less output and more docs

--- switch-layout.py
+++ switch-layout.py
@@ -3,12 +3,20 @@ import subprocess
 
 from pynput import keyboard
 
+# The shortcuts triggering switch
 SWITCH_SHORTCUTS = [
+    # For some reason Alt pressed after Shift gets registered as a different
+    # key code on my system.
     {keyboard.KeyCode(65511)},  # Shift + Alt
     {keyboard.Key.alt, keyboard.Key.shift},  # Alt + Shift
+
+    # Examples
+    # {keyboard.Key.shift, keyboard.Key.ctrl},  # Shift + Ctrl
+    # {keyboard.Key.cmd, keyboard.Key.space},  # Super + Space
 ]
 
-LAYOUTS = ['us', 'ru']
+# How many layouts do you have?
+LAYOUTS_COUNT = 2
 
 
 class Switcher:
@@ -52,10 +60,8 @@ class Switcher:
         return False
 
     def on_switch(self):
-        print("Switch")
-
         self.current_layout += 1
-        if self.current_layout >= len(LAYOUTS):
+        if self.current_layout >= LAYOUTS_COUNT:
             self.current_layout = 0
 
         command = [
@@ -65,9 +71,8 @@ class Switcher:
             "current",
             str(self.current_layout),
         ]
-        exitcode = subprocess.call(
+        _exitcode = subprocess.call(
             command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print(exitcode)
 
 
 def main():
